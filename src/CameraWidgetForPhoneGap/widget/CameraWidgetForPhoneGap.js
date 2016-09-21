4e5af1c3e0c8b052b1876dbcc6364ffc4ef612f2
Marcel Groeneweg <marcel.groeneweg@itvisors.com>
2016-09-21 11:02:35 +0200

    Open camera or gallery automatically Upgraded test project to Mx5.21.5 because it choked on the offlineCapable attribute in the widget XML Enhanced test project to open camera or gallery.

--- src/CameraWidgetForPhoneGap/widget/CameraWidgetForPhoneGap.js
+++ src/CameraWidgetForPhoneGap/widget/CameraWidgetForPhoneGap.js
@@ -1,10 +1,12 @@
+/*jslint browser:true, nomen: true */
+/*global mx, define, require, console, Camera, FileReader, Blob, FileUploadOptions, FileTransfer */
 /*
  * Copyright 2014-2016 Mendix Technology bv
  */
 require([
     "dojo/_base/declare", "mxui/widget/_WidgetBase", "dijit/_TemplatedMixin",
-    "mxui/dom", "dojo/dom-class", "dojo/dom-style", "dojo/dom-construct"
-], function(declare, _WidgetBase, _TemplatedMixin, dom, domClass, domStyle, domConstruct) {
+    "mxui/dom", "dojo/dom-class", "dojo/dom-style", "dojo/dom-construct", "dojo/_base/lang"
+], function (declare, _WidgetBase, _TemplatedMixin, dom, domClass, domStyle, domConstruct, dojoLang) {
 
     "use strict";
 
@@ -21,26 +23,30 @@ require([
         autoSaveEnabled: false,
         onchangemf: "",
         pictureSource: "camera",
+        autoStartDelay: 0,
 
         _contextObj: null,
         _imageUrl: null,
         _previewNode: null,
 
 
-        postCreate: function() {
+        postCreate: function () {
+            var elements,
+                alignment;
+            
             domClass.add(this.domNode, "wx-CameraWidgetForPhoneGap");
 
-            var elements = [ this._setupPreview(), this._setupButton() ];
+            elements = [ this._setupPreview(), this._setupButton() ];
             if (/below|right/.test(this.imageLocation)) {
                 elements.reverse();
             }
 
-            var alignment = "horizontal";
+            alignment = "horizontal";
             if (/above|below/.test(this.imageLocation)) {
                 alignment = "vertical";
             }
 
-            elements.forEach(function(el) {
+            elements.forEach(function (el) {
                 domClass.add(el, "wx-CameraWidgetForPhoneGap-" + alignment);
                 this.domNode.appendChild(el);
             }, this);
@@ -48,18 +54,23 @@ require([
             this.listen("save", this._sendFile);
         },
 
-        update: function(obj, callback) {
+        update: function (obj, callback) {
             if (obj) {
                 this._contextObj = obj;
                 this._loadData();
                 this._resetSubscriptions();
                 this._setPicture("");
+                if (this.autoStartDelay > 0) {
+                    setTimeout(dojoLang.hitch(this, this._getPicture), this.autoStartDelay);
+                }
             }
 
-            if (callback) callback();
+            if (callback) {
+                callback();
+            }
         },
 
-        _setupButton: function() {
+        _setupButton: function () {
             var button = dom.create("button", {
                 "type": "button",
                 "class": "btn btn-primary wx-CameraWidgetForPhoneGap-button " + this.buttonClass
@@ -69,20 +80,20 @@ require([
             return button;
         },
 
-        _setupPreview: function() {
+        _setupPreview: function () {
             this._previewNode = dom.create("div", {
                 "class": "wx-CameraWidgetForPhoneGap-preview " + this.imageContainerClass
             });
             return this._previewNode;
         },
 
-        _loadData: function() {
+        _loadData: function () {
             if (this._contextObj) {
                 if (!this._contextObj.inheritsFrom("System.FileDocument")) {
                     var span = dom.create("span", {
                             "class": "alert-danger"
                         },
-                        "Entity '" + this._contextObj.getEntity() + "' does not inherit from 'System.FileDocument'");
+                            "Entity '" + this._contextObj.getEntity() + "' does not inherit from 'System.FileDocument'");
                     domConstruct.empty(this.domNode);
                     this.domNode.appendChild(span);
                 }
@@ -90,12 +101,12 @@ require([
 
         },
 
-        _setPicture: function(url) {
+        _setPicture: function (url) {
             this._imageUrl = url;
             this._setThumbnail(url);
         },
 
-        _setThumbnail: function(url) {
+        _setThumbnail: function (url) {
             var urlDisplay = url ? "" : "none",
                 width = this.imageWidth || 100,
                 height = this.imageHeight || 100,
@@ -109,26 +120,30 @@ require([
             });
         },
 
-        _getPicture: function() {
-            var self = this;
+        _getPicture: function () {
+            var self = this,
+                sourceType,
+                params;
 
             if (!navigator.camera) {
                 window.mx.ui.error("Unable to detect camera.");
                 return;
             }
 
-            var sourceType = (this.pictureSource == "camera") ?
+            sourceType = (this.pictureSource === "camera") ?
                     Camera.PictureSourceType.CAMERA : Camera.PictureSourceType.PHOTOLIBRARY;
-            var params = {
+            params = {
                 quality: 50,
                 destinationType: Camera.DestinationType.FILE_URL,
                 correctOrientation: true,
                 sourceType: sourceType
             };
-            if (this.targetWidth !== 0) params.targetWidth = this.targetWidth;
-            if (this.targetHeight !== 0) params.targetHeight = this.targetHeight;
-            // TODO: get rid of temp image files
-            navigator.camera.getPicture(success, error, params);
+            if (this.targetWidth !== 0) {
+                params.targetWidth = this.targetWidth;
+            }
+            if (this.targetHeight !== 0) {
+                params.targetHeight = this.targetHeight;
+            }
 
             function success(url) {
                 if (self.autoSaveEnabled) {
@@ -143,47 +158,35 @@ require([
                     window.mx.ui.error("Retrieving image from camera failed with error code " + e.code);
                 }
             }
+            // TODO: get rid of temp image files
+            navigator.camera.getPicture(success, error, params);
         },
 
-        _sendFile: function(callback) {
-            var self = this;
+        _sendFile: function (callback) {
+            var self = this,
+                filename,
+                guid,
+                options,
+                url,
+                ft;
 
             if (!this._imageUrl) {
-                if (callback) callback();
+                if (callback) {
+                    callback();
+                }
                 return;
             }
 
-            var filename = /[^\/]*$/.exec(this._imageUrl)[0];
-            var guid = this._contextObj.getGuid();
-            if (window.mx.data.saveDocument && window.mx.data.saveDocument.length === 6) {
-                window.resolveLocalFileSystemURL(this._imageUrl, function(fileEntry) {
-                    fileEntry.file(function(blob) {
-                        var fileReader = new FileReader();
-                        fileReader.onload = function(e) {
-                            window.mx.data.saveDocument(guid, filename, {}, new Blob([ e.target.result ]), success, error);
-                        };
+            function success() {
+                self._setPicture("");
+                self._executeMicroflow();
+                if (callback) {
+                    callback();
+                }
+            }
 
-                        fileReader.onerror = function(e) {
-                            error(e.target.error);
-                        };
-
-                        fileReader.readAsArrayBuffer(blob);
-                    }, error);
-                }, error);
-            } else {
-                // For Mendix versions < 6.4
-                var options = new FileUploadOptions();
-                options.fileKey = "mxdocument";
-                options.fileName = filename;
-                options.mimeType = "image/jpeg";
-                options.useBrowserHttp = true;
-
-                var url = mx.appUrl +
-                    "file?guid=" + this._contextObj.getGuid() +
-                    "&csrfToken=" + mx.session.getCSRFToken();
-
-                var ft = new FileTransfer();
-                ft.upload(this._imageUrl, url, refreshObject, error, options);
+            function error(e) {
+                window.mx.ui.error("Uploading image failed with error code " + e.code);
             }
 
             function refreshObject() {
@@ -195,39 +198,64 @@ require([
                 });
             }
 
-            function success() {
-                self._setPicture("");
-                self._executeMicroflow();
-                if (callback) callback();
+            filename = /[^\/]*$/.exec(this._imageUrl)[0];
+            guid = this._contextObj.getGuid();
+            if (window.mx.data.saveDocument && window.mx.data.saveDocument.length === 6) {
+                window.resolveLocalFileSystemURL(this._imageUrl, function (fileEntry) {
+                    fileEntry.file(function (blob) {
+                        var fileReader = new FileReader();
+                        fileReader.onload = function (e) {
+                            window.mx.data.saveDocument(guid, filename, {}, new Blob([ e.target.result ]), success, error);
+                        };
+
+                        fileReader.onerror = function (e) {
+                            error(e.target.error);
+                        };
+
+                        fileReader.readAsArrayBuffer(blob);
+                    }, error);
+                }, error);
+            } else {
+                // For Mendix versions < 6.4
+                options = new FileUploadOptions();
+                options.fileKey = "mxdocument";
+                options.fileName = filename;
+                options.mimeType = "image/jpeg";
+                options.useBrowserHttp = true;
+
+                url = mx.appUrl +
+                    "file?guid=" + this._contextObj.getGuid() +
+                    "&csrfToken=" + mx.session.getCSRFToken();
+
+                ft = new FileTransfer();
+                ft.upload(this._imageUrl, url, refreshObject, error, options);
             }
 
-            function error(e) {
-                window.mx.ui.error("Uploading image failed with error code " + e.code);
-            }
+
         },
 
-        _autoSave: function(url) {
+        _autoSave: function (url) {
             this._imageUrl = url;
-            if (this._contextObj){
-                 window.mx.data.save({
-                     mxobj: this._contextObj,
-                     callback: function(){
+            if (this._contextObj) {
+                window.mx.data.save({
+                    mxobj: this._contextObj,
+                    callback: function () {
                         this._sendFile();
-                     }
+                    }
                 }, this);
             }
         },
 
-        _resetSubscriptions: function() {
+        _resetSubscriptions: function () {
             this.unsubscribeAll();
 
             if (this._contextObj) {
                 this.subscribe({
                     guid: this._contextObj.getGuid(),
-                    callback: function(guid) {
+                    callback: function (guid) {
                         window.mx.data.get({
                             guid: guid,
-                            callback: function(obj) {
+                            callback: function (obj) {
                                 this._contextObj = obj;
                                 this._loadData();
                             }
@@ -237,7 +265,7 @@ require([
             }
         },
 
-        _executeMicroflow: function() {
+        _executeMicroflow: function () {
             if (this.onchangemf && this._contextObj) {
                 window.mx.data.action({
                     params: {
@@ -245,10 +273,10 @@ require([
                         applyto: "selection",
                         guids: [ this._contextObj.getGuid() ]
                     },
-                    callback: function(objs) {
+                    callback: function (objs) {
                         //ok
                     },
-                    error: function(e) {
+                    error: function (e) {
                         console.warn("Error running microflow: ", e);
                     }
                 });
